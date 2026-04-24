cf7c00c67f3a6804b8f430c2cf89656c5f828bb6
NadiiaKuz <nadink1603@gmail.com>
2026-04-24 11:59:41 +0200

    added required functionality

---
+++ frontend/src/components/Assignment.jsx
@@ -0,0 +1,9 @@
+export default function Assignment({a}) {
+    
+    return (
+        <article>
+            <h3>{a.assignmentname}</h3>
+            <p>{a.assignmentdescription}</p>
+        </article>
+    )
+}
--- frontend/src/pages/Student.jsx
+++ frontend/src/pages/Student.jsx
@@ -1,17 +1,36 @@
-import Assignments from "../components/Assignments";
+import { useEffect, useState } from "react";
+import client from "../helpers/client";
+
+import Assignment from "../components/Assignment";
 
 export default function Student() {
+    const [sanityAssignments, setSanityAssignments] = useState(null)
+
+    useEffect(() => {
+        async function fetchAllAssignments() {
+            const allAssignments = await client.fetch("*[_type == 'UINAssignment']")
+            setSanityAssignments(allAssignments)
+        }
+
+        fetchAllAssignments()
+    }, [])
+
+    console.log(sanityAssignments)
+
     return (
         <main>
             <h1>Student</h1>
             <img src="../website_images/Bobby.jpg" alt="Nadiia Kuz" />
             <section>
-                <p>Nadiia Kuz</p>
+                <h2>Nadiia Kuz</h2>
                 <address>
                     <p>E-post: <a href="mailto:nadiia.kuz@example.com">nadiia.kuz@example.com</a></p>
                 </address>
                 <p>Bachelorstudie: Informasjonssystemer, 2. år </p>
-                <Assignments />
+            </section>
+            <section>
+                <h2>Oversikt over arbeidskrav fra UIN</h2>
+                {sanityAssignments?.map(a => <Assignment key={a._id} a={a} />)}
             </section>
         </main>
     )
